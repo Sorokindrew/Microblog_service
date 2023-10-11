efa546997b992292eac5e24733253300f248090c
Andrey Sorokin <sorokindrew@mail.ru>
2023-10-11 15:47:11 +0300

    Containers for test DB and tests added

--- app/tests/test_app.py
+++ app/tests/test_app.py
@@ -1,8 +1,30 @@
+import pytest
+
+
 def test_working_test(client):
     assert 1 == 1
 
 
 def test_users_me(client):
     response = client.get('api/users/me')
-    print(response.json['user']['name'])
+    assert response.json['user']['name'] == 'Andrey Sorokin'
+
+
+@pytest.mark.parametrize('route', ['api/users/me',
+                                   'api/users/1',
+                                   'api/tweets'])
+def test_get_response(client, route):
+    response = client.get(route)
     assert response.status_code == 200
+
+
+def test_add_like(client):
+    response = client.post('/api/tweets/1/likes')
+    assert response.json['result'] == 'true'
+
+
+def test_get_tweets(client):
+    response = client.get('/api/tweets')
+    data = response.json
+    assert data['result'] == 'true'
+    assert len(data['tweets']) == 41
